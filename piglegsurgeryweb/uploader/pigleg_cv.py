1af5c82d90a020960d8f640e7436bed09debf019
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-10-05 09:35:34 +0200

    cv api

---
+++ piglegsurgeryweb/uploader/pigleg_cv.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+def run_video_processing(filename:Path, outputdir:Path) -> dict:
+    #TODO here will be tracking
+    return {
+        "Needle Holder Tip Track Length [m]": 123.5,
+        "Needle Holder Tip Avg Velocity [ms^1]": 123.5,
+    }
+
+
+def run_image_processing(filename:Path, outputdir:Path) -> dict:
+    # TODO here will be angle measurement
+    return {
+        "Stitch Angle 1 [°]": 0.75,
+        "Stitch Angle 2 [°]": 0.75,
+    }
+    pass
