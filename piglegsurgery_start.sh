1b2f4f2a3cbab5aada54b0ed97ef65da50a708e6
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-10-09 21:41:45 +0200

    run qcluster and web server

--- piglegsurgery_start.sh
+++ piglegsurgery_start.sh
@@ -2,6 +2,6 @@
 
 cd /webapps/piglegsurgery/piglegsurgeryweb
 
-conda run -n piglegsurgery --no-capture-output python manage.py qcluster >> log_qcluster.txt&
+conda run -n piglegsurgery --no-capture-output python manage.py qcluster &>> log_qcluster.txt&
 
-conda run -n piglegsurgery --no-capture-output gunicorn piglegsurgeryweb.wsgi:application --bind 0:8001 >> log_qcluster.txt&
+conda run -n piglegsurgery --no-capture-output gunicorn piglegsurgeryweb.wsgi:application --bind 0:8001 &>> log_qcluster.txt&
