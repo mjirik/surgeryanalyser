0f12a424d741105943ce4744604930bf4f24df6e
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-10-09 21:57:13 +0200

    echo path

--- piglegsurgery_start.sh
+++ piglegsurgery_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+echo ${PATH}
 cd /webapps/piglegsurgery/piglegsurgeryweb
-
 conda run -n piglegsurgery --no-capture-output python manage.py qcluster &>> log_qcluster.txt&
-
 conda run -n piglegsurgery --no-capture-output gunicorn piglegsurgeryweb.wsgi:application --bind 0:8000 &>> log_gunicorn.txt&
