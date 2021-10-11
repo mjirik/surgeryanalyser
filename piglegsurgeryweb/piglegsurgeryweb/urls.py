eb6300c01fbea8181c99ef784a2da31e1e022032
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-10-11 21:04:42 +0200

    downloadable zip

--- piglegsurgeryweb/piglegsurgeryweb/urls.py
+++ piglegsurgeryweb/piglegsurgeryweb/urls.py
@@ -18,6 +18,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 # from ..uploader import views
 
@@ -37,3 +39,4 @@ urlpatterns = [
 urlpatterns += (
     staticfiles_urlpatterns()
 )  # used for static files with gunicorn not for production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
