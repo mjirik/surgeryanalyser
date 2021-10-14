116f3f7bda7837627147a5b852fcb0f63256b924
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-10-14 20:52:25 +0200

    path to video is not relative now

--- tests/general_test.py
+++ tests/general_test.py
@@ -8,6 +8,7 @@ import sys
 # pigleg_pth = rootdir/ "piglegsurgeryweb"
 # sys.path.insert(0, str(pigleg_pth))
 from loguru import logger
+local_dir = Path(__file__).parent
 
 
 def test_pigleg_cv():
@@ -18,7 +19,7 @@ def test_pigleg_cv():
     # media_pth = Path(
     #     r"H:\biomedical\orig\pigleg_surgery\first_dataset\b6c6fb92-d8ad-4ccf-994c-5241a89a9274.mp4"
     # )
-    media_pth = Path("pigleg_test.mp4")
+    media_pth = local_dir / "pigleg_test.mp4"
     assert media_pth.exists()
     outputdir = Path("test_pigleg_cv_output")
     pigleg_cv.run_media_processing(media_pth, outputdir)
