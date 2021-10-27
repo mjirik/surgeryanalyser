d0c422e67bae662ebdd06505719383ea17d8c485
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-10-27 11:08:06 +0200

    CRLF -> LF

--- piglegcv/run_services.sh
+++ piglegcv/run_services.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 sudo service redis-server start &>> logs/redis_log.txt
 python worker.py &>> logs/worker_log.txt &
 python app.py &>> logs/app_log.txt &
--- tests/piglegcv_rest_api_test.py
+++ tests/piglegcv_rest_api_test.py
@@ -4,13 +4,13 @@ from loguru import logger
 
 
 def test_pigleg_cv_rest_api():
-    # query = {"filename": "/webapps/piglegsurgeryweb/tests/pigleg_test.mp4"}
-    # response = requests.post('http://127.0.0.1:5000/exists', params=query)
-    # logger.debug(response)
+    query = {"filename": "/webapps/piglegsurgeryweb/tests/pigleg_test.mp4"}
+    response = requests.post('http://127.0.0.1:5000/exists', params=query)
+    logger.debug(response)
 
-    # query = {'lat': '45', 'lon': '180'}
-    # # response = requests.get('http://api.open-notify.org/iss-pass.json', params=query)
-    query = {"filename": "/webapps/piglegsurgeryweb/tests/pigleg_test.mp4lk", "outputdir": "/webapps/piglegsurgeryweb/tests/outputdir"}
-    response = requests.post('http://127.0.0.1:5000/run', params=query)
-    print(response.json())
-    # response = requests.get("http://api.open-notify.org/astros.json")
+    # # query = {'lat': '45', 'lon': '180'}
+    # # # response = requests.get('http://api.open-notify.org/iss-pass.json', params=query)
+    # query = {"filename": "/webapps/piglegsurgeryweb/tests/pigleg_test.mp4lk", "outputdir": "/webapps/piglegsurgeryweb/tests/outputdir"}
+    # response = requests.post('http://127.0.0.1:5000/run', params=query)
+    # print(response.json())
+    # # response = requests.get("http://api.open-notify.org/astros.json")
