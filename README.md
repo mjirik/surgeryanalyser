180dd92b59033e4cb86f9b6de42d6b176686c04c
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-01-19 12:14:33 +0100

    increased timeout of web server

--- README.md
+++ README.md
@@ -148,7 +148,7 @@ python manage.py runserver 0:8000 |& tee -a ~/piglegcv/logs/runserver_log.txt
 ```
 or run server for production (multithreaded)
 ```bash
-gunicorn piglegsurgeryweb.wsgi:application --bind 0:8000 --timeout 150 --workers 5 |& tee -a ~/piglegcv/logs/gunicorn_log.txt
+gunicorn --log-level debug piglegsurgeryweb.wsgi:application --bind 0:8000 --timeout 1800 --workers 5 |& tee >(rotatelogs -n 3 /webapps/piglegsurgery/piglegsurgeryweb/log/gunicorn_log.txt.bck 1k) | tee  >(rotatelogs -n 1 /webapps/piglegsurgery/piglegsurgeryweb/log/gunicorn_log.txt 1M)
 ```
 
 
--- docker/start.sh
+++ docker/start.sh
@@ -14,4 +14,9 @@ echo "  QCluster started"
 conda run -n piglegsurgery --no-capture-output python manage.py runserver 0:8000 |& \
     tee >(rotatelogs -n 1 ~/piglegcv/logs/runserver_log.txt 1M) | \
     tee >(rotatelogs -n 3 ~/piglegcv/logs/runserver_log.txt.bck 1M) &
+echo "  Django (debug) webserver started"
+
+# TODO production run
+#gunicorn --log-level debug piglegsurgeryweb.wsgi:application --bind 0:8000 --timeout 1800 --workers 5 |& \
+#    tee >(rotatelogs -n 3 /webapps/piglegsurgery/piglegsurgeryweb/log/gunicorn_log.txt.bck 1k) | tee  >(rotatelogs -n 1 /webapps/piglegsurgery/piglegsurgeryweb/log/gunicorn_log.txt 1M)
 echo "Services started"
