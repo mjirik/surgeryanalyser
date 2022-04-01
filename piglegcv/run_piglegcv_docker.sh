174a622188a57f8e766f6cbe1af7ca6dc63402c6
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-04-01 13:03:58 +0200

    home instead of ~

--- piglegcv/run_piglegcv_docker.sh
+++ piglegcv/run_piglegcv_docker.sh
@@ -1,6 +1,6 @@
 DATADIR="/webapps/piglegsurgery/piglegsurgeryweb/media"
 #LOGDIR="/webapps/piglegsurgery/piglegsurgeryweb/log"
-LOGDIR="~/piglegcv/logs"
+LOGDIR="$HOME/piglegcv/logs"
 mkdir -p $LOGDIR
 docker run --gpus all -d --rm \
   --shm-size=8gb --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
