4a76e7dfdb807f302eb2d941eab9dc8243383169
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-04-01 12:21:12 +0200

    checking docker

--- docker/start.sh
+++ docker/start.sh
@@ -4,10 +4,15 @@ echo "Starting services..."
 echo "PATH=${PATH}"
 mkdir -p ~/piglegcv/logs/
 cd /webapps/piglegsurgery/piglegsurgeryweb
-sudo service redis-server start |& \
-    tee >(rotatelogs -n 1 ~/piglegcv/logs/redis_log.txt 1M) | \
-    tee >(rotatelogs -n 3 ~/piglegcv/logs/redis_log.txt.bck 1M)
-echo "  Redis started"
+if [ -f /.dockerenv ]; then
+    echo "I'm inside docker";
+    sudo service redis-server start |& \
+        tee >(rotatelogs -n 1 ~/piglegcv/logs/redis_log.txt 1M) | \
+        tee >(rotatelogs -n 3 ~/piglegcv/logs/redis_log.txt.bck 1M)
+    echo "  Redis started"
+else
+    echo "I'm living in real world!";
+fi
 conda run -n piglegsurgery --no-capture-output python manage.py qcluster |& \
     tee >(rotatelogs -n 1 ~/piglegcv/logs/qcluster_log.txt 1M) | \
     tee >(rotatelogs -n 3 ~/piglegcv/logs/qcluster_log.txt.bck 1M) &
