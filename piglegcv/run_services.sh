06c79d5f38b5163ea991af60e8916feeb479cc05
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-04-02 00:44:00 +0200

    fix env use in .sh

--- piglegcv/run_services.sh
+++ piglegcv/run_services.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo "DOCKERLOGNAME=$DOCKERLOGNAME"
+echo "DOCKERLOGNAME={$DOCKERLOGNAME}"
 # there is primary log in .txt file and 3 rotating backup logs
 sudo service redis-server start 2>&1 | \
-   tee >(rotatelogs -n 3 logs/piglegcv_$DOCKERLOGNAME_redis_log.txt.bck 1M) | \
-   rotatelogs -n 1 logs/piglegcv_$DOCKERLOGNAME_redis_log.txt 1M
+   tee >(rotatelogs -n 3 logs/piglegcv_{$DOCKERLOGNAME}_redis_log.txt.bck 1M) | \
+   rotatelogs -n 1 logs/piglegcv_{$DOCKERLOGNAME}_redis_log.txt 1M
 python worker.py 2>&1 | \
-   tee >(rotatelogs -n 3 logs/worker_$DOCKERLOGNAME_log_0.txt.bck 1M) | \
-   rotatelogs -n 1 logs/worker_$DOCKERLOGNAME_log_0.txt 1M &
+   tee >(rotatelogs -n 3 logs/worker_{$DOCKERLOGNAME}_log_0.txt.bck 1M) | \
+   rotatelogs -n 1 logs/worker_{$DOCKERLOGNAME}_log_0.txt 1M &
 python worker.py 2>&1 | \
-   tee >(rotatelogs -n 3 logs/worker_$DOCKERLOGNAME_log_1.txt.bck 1M) | \
-   rotatelogs -n 1 logs/worker_$DOCKERLOGNAME_log_1.txt 1M &
+   tee >(rotatelogs -n 3 logs/worker_{$DOCKERLOGNAME}_log_1.txt.bck 1M) | \
+   rotatelogs -n 1 logs/worker_{$DOCKERLOGNAME}_log_1.txt 1M &
 python worker.py 2>&1 | \
-   tee >(rotatelogs -n 3 logs/worker_$DOCKERLOGNAME_log_2.txt.bck 1M) | \
-   rotatelogs -n 1 logs/worker_$DOCKERLOGNAME_log_2.txt 1M &
+   tee >(rotatelogs -n 3 logs/worker_{$DOCKERLOGNAME}_log_2.txt.bck 1M) | \
+   rotatelogs -n 1 logs/worker_{$DOCKERLOGNAME}_log_2.txt 1M &
 
 python app.py 2>&1 | \
-   tee >(rotatelogs -n 3 logs/app_$DOCKERLOGNAME_log.txt.bck 1M) | \
-   rotatelogs -n 1 logs/app_$DOCKERLOGNAME_log.txt 1M &
+   tee >(rotatelogs -n 3 logs/app_{$DOCKERLOGNAME}_log.txt.bck 1M) | \
+   rotatelogs -n 1 logs/app_{$DOCKERLOGNAME}_log.txt 1M &
 echo "Services started"
