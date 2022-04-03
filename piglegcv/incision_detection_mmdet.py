73334ea3c3c32988ba490be01917541385d5e536
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-04-04 00:57:37 +0200

    opencv does not accept Path

--- piglegcv/incision_detection_mmdet.py
+++ piglegcv/incision_detection_mmdet.py
@@ -228,7 +228,7 @@ def run_incision_detection(img_fn:Path, local_output_data_dir:Path):
 
         imcr = img[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
 
-        cv2.imwrite(local_output_data_dir / f'incision_crop_{i}.jpg', imcr)
+        cv2.imwrite(str(local_output_data_dir / f'incision_crop_{i}.jpg'), imcr)
         imgs.append(imcr)
         # plt.imshow(imcr[:, :, ::-1])
     # predict_image_with_cfg(cfg, model, img_fn, local_output_data_dir)
