a220bf37c98553c2c3dd1ed171ffdb623a3778b9
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-04-03 11:07:32 +0200

    look for the scissors

--- piglegcv/run_qr.py
+++ piglegcv/run_qr.py
@@ -23,7 +23,10 @@ def main_qr(filename, output_dir):
     img_first = None
     box = []
     qr_text = None
+    qr_scissors_frame = []
+    i = -1
     while cap.isOpened():
+        i += 1
         ret, img = cap.read()
         if not ret:
             break
@@ -31,16 +34,25 @@ def main_qr(filename, output_dir):
         #try read QR code
         grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         res = decode(grey)
-        logger.debug(res)
+        # logger.debug(res)
         # if len(res) > 0:
         for oneqr in res:
             txt = oneqr.data.decode("utf8")
-            logger.debug(f"qr code text = {txt}")
+            logger.debug(f"qr code text = '{txt}', frame={i}")
             if txt == "Resolution 30 mm":
                 qr_size = 0.030
+                qr_text = txt
             elif txt == "QR scale pigleg":
                 qr_size = 0.027
+                qr_text = txt
+            elif txt == "Scissors 30 mm":
+                qr_scissors_frame.append[i]
+                if qr_text is None:
+                    # Use only if no Scale QR code was detected
+                    qr_size = 0.030
+                    qr_text = txt
             else:
+                logger.debug(f"Unknown QR code with text='{txt}', on frame={i}")
                 continue
             is_detected = 1
             a = np.array(oneqr.polygon[0])
@@ -58,6 +70,7 @@ def main_qr(filename, output_dir):
     qr_data['pix_size'] = pix_size
     qr_data['qr_size'] = qr_size
     qr_data['text'] = qr_text
+    qr_data['qr_scissors_frame'] = qr_scissors_frame
 
     # save QR to the json file
     save_json({"qr_data": qr_data}, os.path.join(output_dir, "qr_data.json"))
