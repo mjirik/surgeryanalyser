5f0f1b8dd24e85da4dc9039c6e85254c670af510
DNN <mjirik@ntis.zcu.cz>
2022-05-13 11:57:31 +0200

    readme added

---
+++ piglegcv/README.md
@@ -0,0 +1,7 @@
+
+
+
+
+```bash
+docker build -t piglegcv .
+```
--- piglegcv/run_piglegcv_docker_development.sh
+++ piglegcv/run_piglegcv_docker_development.sh
@@ -2,7 +2,7 @@ DATADIR="/webapps/piglegsurgery/piglegsurgeryweb/media"
 #LOGDIR="/webapps/piglegsurgery/piglegsurgeryweb/log_devel"
 LOGDIR="$HOME/piglegcv/logs"
 mkdir -p $LOGDIR
-docker run --gpus all -d --rm \
+docker run --gpus all --privileged=true -d --rm \
   --shm-size=8gb --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   -v $DATADIR:/webapps/piglegsurgery/piglegsurgeryweb/media \
   -v $LOGDIR:/home/appuser/logs \
