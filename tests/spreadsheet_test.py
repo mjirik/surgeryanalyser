7562c849e253be7236e0d245787d6f769cb03f49
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-05-23 00:28:16 +0200

    insert data into google spreadsheet

--- tests/spreadsheet_test.py
+++ tests/spreadsheet_test.py
@@ -18,7 +18,26 @@ def test_spreadsheet():
     client = gspread.authorize(creds)
 
     # get the instance of the Spreadsheet
-    sheet = client.open("https://docs.google.com/spreadsheets/d/1G55kXxcJ0PVaCApsDUJPGRkgoGEpiLaKn0sddRU-jzU/edit#gid=0")
+    sheet = client.open("Pigleg Surgery Stats")
 
     # get the first sheet of the Spreadsheet
     sheet_instance = sheet.get_worksheet(0)
+
+    # get all the records of the data
+    records_data = sheet_instance.get_all_records()
+
+    # convert the json to dataframe
+    records_df = pd.DataFrame.from_dict(records_data)
+
+    # view the top records
+    records_df.head()
+
+    # number of runs by each batsman
+    runs = records_df.groupby(['Prvni sloupec'])['Druhy'].count().reset_index()
+    runs
+
+
+    print(records_data.values.to_list())
+    print("asdf")
+
+    sheet_instance.append_rows([['dd', 5]])
