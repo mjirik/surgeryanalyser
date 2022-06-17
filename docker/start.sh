0159ee7c2495baf17e29eefe09beee7c3d2b7248
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-06-17 17:03:28 +0200

    less loging

--- docker/start.sh
+++ docker/start.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 echo "Starting services..."
+echo "HOME=${HOME}"
 echo "PATH=${PATH}"
 mkdir -p ~/pigleg/logs/
 cd /webapps/piglegsurgery/piglegsurgeryweb
 if [ -f /.dockerenv ]; then
     echo "I'm inside docker";
     sudo service redis-server start |& \
-        tee >(rotatelogs -n 1 ~/pigleg/logs/redis_${DOCKERLOGNAME}_log.txt 1M) | \
-        tee >(rotatelogs -n 3 ~/pigleg/logs/redis_${DOCKERLOGNAME}_log.txt.bck 1M)
+#        tee >(rotatelogs -n 3 ~/pigleg/logs/redis_${DOCKERLOGNAME}_log.txt.bck 1M) | \
+        tee >(rotatelogs -n 1 ~/pigleg/logs/redis_${DOCKERLOGNAME}_log.txt 1M)
     echo "  Redis started"
 else
     echo "Not running in docker, redis should be already running.";
 fi
 conda run -n piglegsurgery --no-capture-output python manage.py qcluster |& \
-    tee >(rotatelogs -n 1 ~/pigleg/logs/qcluster_${DOCKERLOGNAME}_log.txt 1M) | \
-    tee >(rotatelogs -n 3 ~/pigleg/logs/qcluster_${DOCKERLOGNAME}_log.txt.bck 1M) &
+#    tee >(rotatelogs -n 3 ~/pigleg/logs/qcluster_${DOCKERLOGNAME}_log.txt.bck 1M) | \
+    tee >(rotatelogs -n 1 ~/pigleg/logs/qcluster_${DOCKERLOGNAME}_log.txt 1M) &
 echo "  QCluster started"
 conda run -n piglegsurgery --no-capture-output python manage.py runserver 0:8000 |& \
-    tee >(rotatelogs -n 1 ~/pigleg/logs/runserver_${DOCKERLOGNAME}_log.txt 1M) | \
-    tee >(rotatelogs -n 3 ~/pigleg/logs/runserver_${DOCKERLOGNAME}_log.txt.bck 1M) &
+#    tee >(rotatelogs -n 3 ~/pigleg/logs/runserver_${DOCKERLOGNAME}_log.txt.bck 1M) | \
+    tee >(rotatelogs -n 1 ~/pigleg/logs/runserver_${DOCKERLOGNAME}_log.txt 1M) &
 echo "  Django (debug) webserver started"
 
 # TODO production run
