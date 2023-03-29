58039f4b922143362d771284e7b58dd31b9cd1da
Miroslav Jirik <miroslav.jirik@gmail.com>
2023-03-29 10:59:48 +0200

    stable password

--- piglegcv/run_services.sh
+++ piglegcv/run_services.sh
@@ -20,7 +20,48 @@ python app.py 2>&1 | \
    rotatelogs -n 1 logs/app_${DOCKERLOGNAME}_log.txt 1M &
 echo "Services started"
 
-cd /webapps/piglegsurgery/ && jupyter lab --no-browser 2>&1 | \
+
+# Run jupyter lab
+
+HOMEDIR=/storage/plzen1/home/$USER # substitute username and path to to your real username and path
+HOSTNAME=`hostname -f`
+JUPYTER_PORT="8808"
+HOMEDIR=`eval echo ~$USER`
+
+#find nearest free port to listen
+isfree=$(netstat -taln | grep $JUPYTER_PORT)
+while [[ -n "$isfree" ]]; do
+    JUPYTER_PORT=$[JUPYTER_PORT+1]
+    isfree=$(netstat -taln | grep $JUPYTER_PORT)
+done
+
+# move into $HOME directory
+cd $HOMEDIR
+if [ ! -f ./.jupyter/jupyter_notebook_config.json ]; then
+   echo "jupyter passwd reset!"
+   mkdir -p .jupyter/
+   #here you can commem=nt randomly generated password and set your password
+   pass=`dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-12` ; echo $pass
+   #pass="SecretPassWord"
+   hash=`singularity exec $SING_IMAGE python -c "from notebook.auth import passwd ; hash = passwd('$pass') ; print(hash)" 2>/dev/null`
+   cat > .jupyter/jupyter_notebook_config.json << EOJson
+{
+  "NotebookApp": {
+      "password": "$hash"
+    }
+}
+EOJson
+  PASS_MESSAGE="Your password was set to '$pass' (without ticks)."
+else
+  PASS_MESSAGE="Your password was already set before."
+fi
+
+cd /webapps/piglegsurgery/ && jupyter lab --no-browser --port $JUPYTER_PORT 2>&1 | \
 #   tee >(rotatelogs -n 3 logs/jupyterlab_${DOCKERLOGNAME}_log_2.txt.bck 1M) | \
    rotatelogs -n 1 logs/jupyterlab_${DOCKERLOGNAME}_log.txt 1M &
 echo "jupyterlab started"
+
+printf "Job with JupiterLab was started.\n\
+Use URL  http://$HOSTNAME:$JUPYTER_PORT\n\
+$PASS_MESSAGE\n\
+You can reset password by deleting file $HOMEDIR/.jupyter/jupyter_notebook_config.json and run job again with this script.\n"
