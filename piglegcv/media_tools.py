aa370cb2a542b71875939d17731a1e01545d85b9
Miroslav Jirik <miroslav.jirik@gmail.com>
2023-10-12 09:28:31 +0200

    refactor visualization tools

--- piglegcv/media_tools.py
+++ piglegcv/media_tools.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 from typing import Optional, Union
-import cv2
 from loguru import logger
 import json
 import subprocess
-from .visualization_tools import crop_square
 import os
+import numpy as np
 
 
 def save_json(data:dict, output_json:Union[str,Path], update:bool=True):
@@ -102,3 +101,20 @@ def convert_avi_to_mp4(avi_file_path, output_name):
          "-vprofile", "high", "-bf", "0", "-strict", "experimental", "-f", "mp4", output_name]
     subprocess.call(s)
     return True
+
+
+def crop_square(frame:np.ndarray)->np.ndarray:
+
+    mn = np.min(frame.shape[:2])
+    sh0 = frame.shape[0]
+    sh1 = frame.shape[1]
+    if sh0 > sh1:
+        st0 = int((sh0/2) - (sh1/2))
+        st1 = 0
+    else:
+        st0 = 0
+        st1 = int((sh1/2) - (sh0/2))
+
+    frame = frame[st0:st0+mn, st1:st1+mn]
+
+    return frame
--- piglegsurgeryweb/uploader/visualization_tools.py
+++ piglegsurgeryweb/uploader/visualization_tools.py
@@ -1,18 +1,4 @@
 import numpy as np
+from .media_tools import crop_square
 
 
-def crop_square(frame:np.ndarray)->np.ndarray:
-
-    mn = np.min(frame.shape[:2])
-    sh0 = frame.shape[0]
-    sh1 = frame.shape[1]
-    if sh0 > sh1:
-        st0 = int((sh0/2) - (sh1/2))
-        st1 = 0
-    else:
-        st0 = 0
-        st1 = int((sh1/2) - (sh0/2))
-
-    frame = frame[st0:st0+mn, st1:st1+mn]
-
-    return frame
