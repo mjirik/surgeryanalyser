eece0efd51861623b221fc0dfb56a432239c2560
Miroslav Jirik <miroslav.jirik@gmail.com>
2023-12-16 12:01:08 +0100

    perceptual hashing and hashing of models to skip repetitive generation

--- piglegcv/run_tracker_bytetrack.py
+++ piglegcv/run_tracker_bytetrack.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from loguru import logger
 
 from mmtrack.apis import inference_mot, init_model
+import tools
 
 def add_tracking_results(tracking_results, result):
     if result != None:
@@ -42,13 +43,19 @@ def main_tracker_bytetrack(
         device=None,
         # score_thr=0.5,
         crop: list=[None, None, None, None],
-        class_names = None,
+        class_names=[],
 ):
     """Run tracking on a video.
     trackers: is list of tuples (config_file, checkpoint)
     """
 
-    hash = ""
+    # make peceptual hash of first video frame
+    imgs = mmcv.VideoReader(str(filename))
+    first_frame = next(imgs)
+    logger.debug(f"{first_frame.shape=}")
+    phash = tools.phash_image(first_frame)
+
+    hash = phash
     models = []
     for tracker_config, tracker_checkpoint in trackers_config_and_checkpoints:
         # build the model from a config file and a checkpoint file
@@ -56,43 +63,36 @@ def main_tracker_bytetrack(
         models.append(model)
         hash += make_hash_from_model(tracker_checkpoint)
 
-    # nemělo by tady být spíš device="cuda" ? To ale nefunguje protože: RuntimeError: nms_impl: implementation for device cuda:0 not found.
-
-    imgs = mmcv.VideoReader(str(filename))
-
-    # build the model from a config file and a checkpoint file
-    # model = init_model(config_file, str(checkpoint), device=device)
-    # model = init_model(config_file, str(checkpoint), device=device)
-    #print(model)
-
-    # test and show/save the images
     run_tracking = True
     if output_file_path.exists():
         data = json.load(open(output_file_path, 'r'))
         if ("hash" in data) and (data['hash'] == hash):
             run_tracking = False
             logger.debug("Tracking results already exists. Skipping tracking.")
+        else:
+            logger.debug(f"Hashes are different: {data['hash']} != {hash}")
 
+    imgs = mmcv.VideoReader(str(filename))
 
+    if run_tracking:
+        tracking_results = {'tracks': [], "hash": hash, "class_names": class_names}
+        for i, img in enumerate(imgs):
+            frame_tr = []
+            if not (i % 50):
+                logger.debug(f'Processing frame {i} by tracker')
+            for j, tracker in enumerate(trackers_config_and_checkpoints):
 
-    tracking_results = {'tracks': [], "hash": hash, "class_names": class_names}
-    for i, img in enumerate(imgs):
-        frame_tr = []
-        if not (i % 50):
-            logger.debug(f'Processing frame {i} by tracker')
-        for j, tracker in enumerate(trackers_config_and_checkpoints):
+                result = inference_mot(tracker, img[crop[0]:crop[1], crop[2]:crop[3], :], frame_id=i)
 
-            result = inference_mot(tracker, img[crop[0]:crop[1], crop[2]:crop[3], :], frame_id=i)
+                # if result != None:
+                # logger.debug(f"{j=}, {result=}")
+                for k, tr in enumerate(result['track_bboxes']):
+                    if len(tr) > 0:
+                        frame_tr.append(tr[0].tolist()[1:] + [k+(j*10)])
+            # logger.debug(f"track_bboxes per frame {frame_tr}")
+            tracking_results['tracks'].append(frame_tr)
 
-            # if result != None:
-            # logger.debug(f"{j=}, {result=}")
-            for k, tr in enumerate(result['track_bboxes']):
-                if len(tr) > 0:
-                    frame_tr.append(tr[0].tolist()[1:] + [k+(j*10)])
-        # logger.debug(f"track_bboxes per frame {frame_tr}")
-        tracking_results['tracks'].append(frame_tr)
+            # add_tracking_results(tracking_results, result)
 
-        # add_tracking_results(tracking_results, result)
-        
-    json.dump(tracking_results, open(output_file_path, 'w'))
+        json.dump(tracking_results, open(output_file_path, 'w'))
 
