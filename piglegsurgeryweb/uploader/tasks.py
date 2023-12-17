b1d74ef2b6867a1b3a3e7ff86754454ccf19b3bf
Miroslav Jirik <miroslav.jirik@gmail.com>
2023-12-17 21:22:02 +0100

    fix create dir before copy backuped track.json

--- piglegsurgeryweb/uploader/tasks.py
+++ piglegsurgeryweb/uploader/tasks.py
@@ -105,8 +105,10 @@ def run_processing(serverfile: UploadedFile, absolute_uri, hostname, port):
             shutil.move(tracks_json_path, tracks_json_tmp_path)
         shutil.rmtree(outputdir, ignore_errors=True)
         if tracks_json_tmp_path.exists():
+            outputdir.mkdir(parents=True, exist_ok=True)
             shutil.move(tracks_json_tmp_path, tracks_json_path)
-    outputdir.mkdir(parents=True, exist_ok=True)
+    else:
+        outputdir.mkdir(parents=True, exist_ok=True)
     log_format = loguru._defaults.LOGURU_FORMAT
     logger_id = logger.add(
         str(Path(serverfile.outputdir) / "webapp_log.txt"),
