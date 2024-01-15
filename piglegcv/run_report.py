fix visibility time calculation

piglegcv/run_report.py
@@ -271,7 +271,8 @@ def create_pdf_report_for_one_tool(
 
         # print(dt)
         L = np.sum(ds)
-        T = np.sum(dt)
+        # T = np.sum(dt)
+        T = len(frame_ids) / float(source_fps)
         logger.debug(f"{object_name=}")
         logger.debug(f"{frame_ids=}")
         logger.debug(f"tool: {T=} , {source_fps=}, {len(frame_ids)=}")