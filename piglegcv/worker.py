cd3979ebb6d88798c9b6ca4960c4e56f97e97597
Miroslav Jirik <miroslav.jirik@gmail.com>
2024-11-12 23:27:24 +0100

    back to rq<2.0.0

--- piglegcv/worker.py
+++ piglegcv/worker.py
@@ -2,7 +2,7 @@ import os
 
 import redis
 from rq import Queue, Worker
-# from rq import Connection
+from rq import Connection
 
 from loguru import logger
 
@@ -13,12 +13,12 @@ redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 conn = redis.from_url(redis_url)
 
 if __name__ == "__main__":
-    logger.info("Starting worker")
-    queues = [Queue(name, connection=conn) for name in listen]
-    worker = Worker(queues, connection=conn)
-    worker.work()
-    # deprecated
-    # with Connection(conn):
-    #     logger.info("Starting worker")
-    #     worker = Worker(list(map(Queue, listen)))
-    #     worker.work()
+    # logger.info("Starting worker")
+    # queues = [Queue(name, connection=conn) for name in listen]
+    # worker = Worker(queues, connection=conn)
+    # worker.work()
+    # deprecated in 2.0.0
+    with Connection(conn):
+        logger.info("Starting worker")
+        worker = Worker(list(map(Queue, listen)))
+        worker.work()
