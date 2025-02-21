b8efcf13063402fe58695b5597e06e383f704f38
Miroslav Jirik <miroslav.jirik@gmail.com>
2025-02-21 11:20:59 +0100

    run gunicorn with production

--- docker/start.sh
+++ docker/start.sh
@@ -26,12 +26,26 @@ conda run -n piglegsurgery --no-capture-output python manage.py qcluster |& \
 #    tee >(rotatelogs -n 3 ~/pigleg/logs/qcluster_${DOCKERLOGNAME}_log.txt.bck 1M) | \
     tee >(rotatelogs -n 1 ~/pigleg/logs/piglegweb_qcluster_${DOCKERLOGNAME}_log.txt 1M) &
 echo "  QCluster started"
-conda run -n piglegsurgery --no-capture-output python manage.py runserver 0:8000 |& \
-#    tee >(rotatelogs -n 3 ~/pigleg/logs/runserver_${DOCKERLOGNAME}_log.txt.bck 1M) | \
-    tee >(rotatelogs -n 1 ~/pigleg/logs/piglegweb_runserver_${DOCKERLOGNAME}_log.txt 1M) &
-echo "  Django (debug) webserver started"
 
+# conda run -n piglegsurgery --no-capture-output python manage.py runserver 0:8000 |& \
+# #    tee >(rotatelogs -n 3 ~/pigleg/logs/runserver_${DOCKERLOGNAME}_log.txt.bck 1M) | \
+#     tee >(rotatelogs -n 1 ~/pigleg/logs/piglegweb_runserver_${DOCKERLOGNAME}_log.txt 1M) &
+# echo "  Django (debug) webserver started"
 # TODO production run
 #gunicorn --log-level debug piglegsurgeryweb.wsgi:application --bind 0:8000 --timeout 1800 --workers 5 |& \
 #    tee >(rotatelogs -n 3 /webapps/piglegsurgery/piglegsurgeryweb/log/gunicorn_log.txt.bck 1k) | tee  >(rotatelogs -n 1 /webapps/piglegsurgery/piglegsurgeryweb/log/gunicorn_log.txt 1M)
+
+
+# Determine whether to run development or production server
+if [ "$ENV" = "production" ]; then
+    echo "Starting Gunicorn for production..."
+    gunicorn --log-level debug piglegsurgeryweb.wsgi:application --bind 0.0.0.0:8000 --timeout 1800 --workers 5 |& \
+    tee >(rotatelogs -n 1 ~/pigleg/logs/piglegweb_gunicorn_log.txt 1M)
+    echo "  Gunicorn started"
+else
+    echo "Starting Django development server..."
+    conda run -n piglegsurgery --no-capture-output python manage.py runserver 0.0.0.0:8000 |& \
+    tee >(rotatelogs -n 1 ~/pigleg/logs/piglegweb_runserver_${DOCKERLOGNAME}_log.txt 1M) &
+    echo "  Django (debug) webserver started"
+fi
 echo "Services started"
