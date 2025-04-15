1925be556b6cc7814f248ca11cec5b40f8a5f68c
Miroslav Jirik <miroslav.jirik@gmail.com>
2025-04-15 16:58:53 +0200

    stitch number increase by on with filters

---
+++ piglegsurgeryweb/uploader/templatetags/myfilter.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def mul(value, arg):
+    """Multiply the value by the argument."""
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return ''
+
+
+@register.filter
+def add(value, arg):
+    """Add the value by the argument."""
+    try:
+        return float(value) + float(arg)
+    except (ValueError, TypeError):
+        return ''
