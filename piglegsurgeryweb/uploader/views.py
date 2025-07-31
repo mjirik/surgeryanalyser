138d0987d601de316cd8cd304ec64c3ed3207abb
Miroslav Jirik <miroslav.jirik@gmail.com>
2025-07-31 10:05:23 +0200

    fix query

--- piglegsurgeryweb/uploader/views.py
+++ piglegsurgeryweb/uploader/views.py
@@ -154,22 +154,20 @@ def report_list(request):
 
 def _general_report_list(request, uploaded_file_set):
     query = request.GET.get("q")
-    words = query.split()
-    q_objects = Q()
-
-    for word in words:
-        word_filter = (
-              Q(email__icontains=word)
-              | Q(mediafile__icontains=word)
-              | Q(collection__name__icontains=word)  # Pokud máš pole s výsledky
-              | Q(category__name__icontains=word)
-        )
-        q_objects &= word_filter  # Každé slovo musí být nalezeno někde
-
-    uploaded_file_set = UploadedFile.objects.filter(q_objects).distinct()
     if query:
-        uploaded_file_set = uploaded_file_set.filter(
-        )
+        words = query.split()
+        q_objects = Q()
+
+        for word in words:
+            word_filter = (
+                  Q(email__icontains=word)
+                  | Q(mediafile__icontains=word)
+                  | Q(collection__name__icontains=word)  # Pokud máš pole s výsledky
+                  | Q(category__name__icontains=word)
+            )
+            q_objects &= word_filter  # Každé slovo musí být nalezeno někde
+
+        uploaded_file_set = uploaded_file_set.filter(q_objects).distinct()
 
     if "order_by" in request.GET:
         logger.debug(f"order_by={request.GET['order_by']}")
